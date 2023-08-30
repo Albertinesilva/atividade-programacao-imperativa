6cd4f91ecf401bff4bf4a101f0284d1195df2d5b
João Ramos <jvitorsb98@outlook.com>
2023-08-30 18:50:26 -0300

    Merge exercicio 2.a

--- exercicio2/a.cpp
+++ exercicio2/a.cpp
@@ -13,17 +13,27 @@ using namespace std;
 
 int main(){
 
-    char num1;
+    char ch0 = '0';
+    char ch1 = '1';
+    char ch2 = '2';
+    char ch3 = '3';
+    char ch4 = '4';
+    char ch5 = '5';
+    char ch6 = '6';
+    char ch7 = '7';
+    char ch8 = '8';
+    char ch9 = '9';
+    
+    cout << "'" << ch0 << "' - " << (int)ch0 << endl;
+    cout << "'" << ch1 << "' - " << (int)ch1 << endl;
+    cout << "'" << ch2 << "' - " << (int)ch2 << endl;
+    cout << "'" << ch3 << "' - " << (int)ch3 << endl;
+    cout << "'" << ch4 << "' - " << (int)ch4 << endl;
+    cout << "'" << ch5 << "' - " << (int)ch5 << endl;
+    cout << "'" << ch6 << "' - " << (int)ch6 << endl;
+    cout << "'" << ch7 << "' - " << (int)ch7 << endl;
+    cout << "'" << ch8 << "' - " << (int)ch8 << endl;
+    cout << "'" << ch9 << "' - " << (int)ch9 << endl;
 
-    cout << " '" << (num1 = '0') << "' "  "- " << int(num1 = '0') << endl;
-    cout << " '" << (num1 = '1') << "' "  "- " << int(num1 = '1') << endl;
-    cout << " '" << (num1 = '2') << "' "  "- " << int(num1 = '2') << endl;
-    cout << " '" << (num1 = '3') << "' "  "- " << int(num1 = '3') << endl;
-    cout << " '" << (num1 = '4') << "' "  "- " << int(num1 = '4') << endl;
-    cout << " '" << (num1 = '5') << "' "  "- " << int(num1 = '5') << endl;
-    cout << " '" << (num1 = '6') << "' "  "- " << int(num1 = '6') << endl;
-    cout << " '" << (num1 = '7') << "' "  "- " << int(num1 = '7') << endl;
-    cout << " '" << (num1 = '8') << "' "  "- " << int(num1 = '8') << endl;
-    cout << " '" << (num1 = '9') << "' "  "- " << int(num1 = '9') << endl;
-
+    return 0 ;
 }
