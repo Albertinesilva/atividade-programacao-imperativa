6738f5fef300443e0614cac22de13f7357551071
João Ramos <jvitorsb98@outlook.com>
2023-08-30 19:59:54 -0300

    atividade terminada

--- exercicio3/a.cpp
+++ exercicio3/a.cpp
@@ -4,6 +4,10 @@ using namespace std;
 
 int main(){
 
+    int maiorInteiroComSinal = 0x7FFFFFFF;
+    
+    cout << "Maior inteiro com sinal : " << maiorInteiroComSinal << endl;
+    cout << "Menor inteiro com sinal : " << (maiorInteiroComSinal+1) << endl;
 
 
 
